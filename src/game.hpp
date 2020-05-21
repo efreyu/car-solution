6274fff88a9e2d1c27099d332c9e2929d3fc4094
Artur Barilo <faustkeller45@gmail.com>
2020-05-22 01:04:40 +0900

    master | remove unused struct

--- src/game.hpp
+++ src/game.hpp
@@ -3,42 +3,6 @@
 #include <cstdlib>
 #include <vector>
 
-struct sPos {
-public:
-    int x;
-    int y;
-    sPos() { x = 0; y = 0; }
-    sPos(int aX, int aY) { x = aX; y = aY; }
-};
-
-struct sSize
-{
-public:
-    int width;
-    int height;
-    sSize() { width = 0; height = 0; }
-    sSize(int aW, int aH) { width = aW; height = aH; }
-};
-
-struct sRect
-{
-public:
-    sPos pos;
-    sSize size;
-    sRect() {};
-    sRect(int x, int y, int w, int h) { pos.x = x; pos.y = y; size.width = w; size.height = h; }
-    bool intersects(const sRect& other) {
-        return other.pos.x + other.size.width >= pos.x &&
-               pos.x + size.width >= other.pos.x &&
-               other.pos.y + other.pos.x >= pos.y &&
-               pos.y + size.height >= other.pos.y;
-//        return !((other.pos.x + other.size.width <= pos.x) ||
-//                 (other.pos.y + other.size.height <= pos.y) ||
-//                 (other.pos.x >= pos.x + size.width) ||
-//                 (other.pos.y >= pos.y + size.height));
-    }
-};
-
 struct sTransform {
 public:
     int x, y, width, height;
--- tests/TestStructMain.cpp
+++ tests/TestStructMain.cpp
@@ -7,26 +7,6 @@ int main(int argc, char** argv) {
     return RUN_ALL_TESTS();
 }
 
-TEST(TestStructMain, TestStructPos) {
-    sPos position1;
-    EXPECT_EQ(position1.x, 0);
-    EXPECT_EQ(position1.y, 0);
-    sPos *position2;
-    position2 = new sPos(5, 5);
-    EXPECT_EQ(position2->x, 5);
-    EXPECT_EQ(position2->y, 5);
-}
-
-TEST(TestStructMain, TestStructSize) {
-    sSize size1;
-    EXPECT_EQ(size1.width, 0);
-    EXPECT_EQ(size1.height, 0);
-    sSize *size2;
-    size2 = new sSize(100, 150);
-    EXPECT_EQ(size2->width, 100);
-    EXPECT_EQ(size2->height, 150);
-}
-
 TEST(TestStructMain, TestStructTransformCollision) {
     sTransform transform1;
     ASSERT_TRUE( transform1.x == 0
