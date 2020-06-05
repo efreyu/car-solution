a9e03d95741214d8a4d82bb8fdafa80a3bfa9225
efreyu <faustkeller45@gmail.com>
2020-06-05 02:48:57 +0000

    Deploying to gh-pages from  @ 8e6a6aab81a2d8daa55d212c58182e5ba60cee9d 🚀

---
+++ CMakeFiles/Car_Example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Car_Example.dir/src/Classes/Game.cpp.o"
+  "CMakeFiles/Car_Example.dir/src/Classes/LoadManager.cpp.o"
+  "CMakeFiles/Car_Example.dir/src/main.cpp.o"
+  "main.js"
+  "main.js.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Car_Example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
