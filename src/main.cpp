70b56fa96fc1af9e4e5fcdaa3cded7e84a26be09
Artur Barilo <faustkeller45@gmail.com>
2020-06-28 18:24:51 +0300

    master | add clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,42 @@
 #include "Classes/Game.h"
 
-Game *game = nullptr;
+Game* game = nullptr;
 
-void game_loop()
-{
-    while (game->isRunning()) {
-        game->GameLoop();
-    }
+void game_loop() {
+  while (game->isRunning()) {
+    game->GameLoop();
+  }
 
-    game->Quit();
+  game->Quit();
 }
 
-void wasm_game_loop()
-{
-    if (game->isRunning()) {
-        game->GameLoop();
-    } else {
-        game->Quit();
+void wasm_game_loop() {
+  if (game->isRunning()) {
+    game->GameLoop();
+  } else {
+    game->Quit();
 #ifdef __EMSCRIPTEN__
-        emscripten_cancel_main_loop();
+    emscripten_cancel_main_loop();
 #endif
-    }
+  }
 }
 
 extern "C" {
 void window_resize(int width, int height) {
-    game->WindowResize(width, height);
+  game->WindowResize(width, height);
 }
 }
 
-int main()
-{
-    game = new Game();
-    game->Init("Car example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, false);
+int main() {
+  game = new Game();
+  game->Init("Car example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+             1024, 768, false);
 
 #ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(wasm_game_loop, 0, 1);
+  emscripten_set_main_loop(wasm_game_loop, 0, 1);
 #else
-    game_loop();
-#endif //__EMSCRIPTEN__
+  game_loop();
+#endif  //__EMSCRIPTEN__
 
-    return 0;
+  return 0;
 }
